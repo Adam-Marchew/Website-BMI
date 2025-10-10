c3e91b34bc607b1b774ecbec24529b24486c9786
Adam-Marchew <adam.marchewka234@gmail.com>
2025-10-10 10:02:13 +0200

    Add the first structure of the BMI calculator

--- Website-BMI/src/app/main/main.ts
+++ Website-BMI/src/app/main/main.ts
@@ -1,11 +1,20 @@
 import { Component} from '@angular/core'
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-main',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './main.html',
   styleUrl: './main.css'
 })
 export class Main {
- 
+
+ height = 0;
+ weight = 0;
+ result = 0;
+
+  bmi(){
+    this.result = this.weight / (this.height * this.height);
+  }
+
 }
