c5a8f8cf533316891059da0d06a6b54b31ce882a
Adam-Marchew <adam.marchewka234@gmail.com>
2025-10-10 12:28:39 +0200

    Fix BMI calculator

--- Website-BMI/src/app/main/main.ts
+++ Website-BMI/src/app/main/main.ts
@@ -9,12 +9,34 @@ import {FormsModule} from '@angular/forms';
 })
 export class Main {
 
- height = 0;
- weight = 0;
- result = 0;
+ height:number = 0;
+ weight:number = 0;
+ result:number = 0;
+ view:number = 0;
+ answer:String = '';
 
-  bmi(){
-    this.result = this.weight / (this.height * this.height);
+  bmi(): void{
+    this.result = this.weight / Math.pow(this.height, 2);
+    this.view = this.result;
+    
+    if(this.result < 16.0){
+      this.answer = "wygłodzenie";
+    } else if (this.result >= 16.0 && this.result <= 16.99){
+      this.answer = "wychudzenie";
+    } else if (this.result >= 17.0 && this.result <= 18.49){
+      this.answer = "niedowaga";
+    } else if (this.result >= 18.5 && this.result <= 24.99){
+      this.answer = "wartość prawidłowa";
+    } else if (this.result >= 25.0 && this.result <= 29.99){
+      this.answer = "nadwaga";
+    } else if (this.result >= 30.0 && this.result <= 34.99){
+      this.answer = "I stopień otyłości";
+    } else if (this.result >= 35.0 && this.result <= 39.99){
+      this.answer = "II stopień otyłości";
+    } else if (this.result >= 40.0){
+      this.answer = "III stopień otyłości";
+    }
+
   }
 
 }
