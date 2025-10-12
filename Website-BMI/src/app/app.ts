6a68b6938312ed461c74bfc7c0d9f83614cffb16
Adam-Marchew <adam.marchewka234@gmail.com>
2025-10-12 14:48:54 +0200

    Add styles to the footer component and fix details in styles

--- Website-BMI/src/app/app.ts
+++ Website-BMI/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
 import { Main } from './main/main';
+import { Footer } from './footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Main],
+  imports: [RouterOutlet, Header, Main, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
